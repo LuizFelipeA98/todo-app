844692f2aec79689adfaa19cf0754570b2f3427c
Luiz Felipe Almeida Santos <luizfelipealmeida82@yahoo.com.br>
2023-12-05 14:09:57 -0300

    salvando uma tarefa no banco de dados

--- index.js
+++ index.js
@@ -7,7 +7,34 @@ const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
-app.unsubscribe(express.static('public'))
+app.use(express.static('public'))
+
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
